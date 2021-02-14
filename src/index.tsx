07ef9af644bb4b584fe883a03457d1dbe66aa769
sjly3k <sjly3k@naver.com>
2021-02-14 15:08:20 +0900

    Commit : Boilerplate 추가

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,14 @@ import { Provider } from "react-redux";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import GlobalStyle from "./styled/globalStyle";
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <ToastContainer position={"top-right"}/>;
+    <GlobalStyle />
+    <ToastContainer position={"top-right"}/>
     <App />
   </Provider>,
   document.getElementById('root')
--- src/lib/apis.ts
+++ src/lib/apis.ts
@@ -1,11 +1,30 @@
 import axios from "axios"
 
+const ACCESS_TOKEN = "G7JcTWd-6AKGY1Zdwg0W6pxnxUydBpptunDjHgopcSEAAAF3nxWXDA";
+const REST_API_KEY = "37d8c5b06ed4992550999ab7c6702671";
+const REDIRECT_URI = "http://localhost:3000/oauth"
+
 const client = axios.create({
-    baseURL : "",
+    baseURL : "https://kapi.kakao.com",
     headers : {
-        "Authorization" : "",
+        "Authorization" : `Bearer ${ACCESS_TOKEN}`,
         "Context-Type" : "application/json; charset=UTF-8"
     }
 })
 
+// 카카오 로그인 /oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code
+export const getUserLogin = async () => {
+    return await client.get(`
+        /oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code    
+    `)
+}
+// 유저 프로필 가져오기 /v1/api/talk/profile
+
+export const getUserProfile = async () => {
+    return await client.get("/v1/api/talk/profile");
+}
+// 유저 친구 목록 가져오기 /v1/api/talk/friends
+export const getUserFriendsList = async () => {
+    return await client.get("/v1/api/talk/friends")
+}
 
